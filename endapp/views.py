a0566a0edd396289503893eef7a5da7202b0fc47
charleslibinkg <129189633+charleslibinkg@users.noreply.github.com>
2023-04-15 17:11:48 +0100

    Update views.py

--- endapp/views.py
+++ endapp/views.py
@@ -4,6 +4,17 @@ from django.http import HttpResponseRedirect
 import MySQLdb
 import datetime
 
+from django.core.files.storage import FileSystemStorage
+db=MySQLdb.connect('localhost','root','','endend')
+c=db.cursor()
+def index(request):
+    return render(request,"index.html")
+def loginn(request):
+    return render(request,"login.html")
+
+                  
+
+  
 
 
 
